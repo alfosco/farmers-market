da255dc61c3bb383e6b182b2ee9944246937432e
Alex Fosco <alex.hf88@gmail.com>
2017-05-11 11:47:57 -0600

    adds price attribute

--- lib/product.rb
+++ lib/product.rb
@@ -1,9 +1,11 @@
 class Product
   attr_reader :name,
-              :code
+              :code,
+              :price
 
-  def initialize(name, code)
+  def initialize(name, code, price)
     @name = name
     @code = code
+    @price = price
   end
 end
--- test/product_test.rb
+++ test/product_test.rb
@@ -4,17 +4,22 @@ require './lib/product'
 
 class ProductTest < Minitest::Test
   def test_product_instantiation
-    product = Product.new("Chai", "CH1")
+    product = Product.new("Chai", "CH1", 3.11)
     assert_instance_of Product, product
   end
 
   def test_it_has_a_name
-    product = Product.new("Chai", "CH1")
+    product = Product.new("Chai", "CH1", 3.11)
     assert_equal "Chai", product.name
   end
 
   def test_it_has_a_code
-    product = Product.new("Chai", "CH1")
+    product = Product.new("Chai", "CH1", 3.11)
     assert_equal "CH1", product.code
+  end
+
+  def test_it_has_a_price
+    product = Product.new("Chai", "CH1", 3.11)
+    assert_equal 3.11, product.price
   end
 end
