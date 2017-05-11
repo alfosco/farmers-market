b9825121001c01283c90cf5581a22b611b99479c
Alex Fosco <alex.hf88@gmail.com>
2017-05-11 12:04:25 -0600

    adds scan method

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -1,3 +1,11 @@
 class Checkout
+  attr_reader :basket
 
+  def initialize
+    @basket = []
+  end
+
+  def scan(product)
+    basket << product
+  end
 end
--- test/checkout_test.rb
+++ test/checkout_test.rb
@@ -1,10 +1,27 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/checkout'
+require './lib/product'
 
 class CheckoutTest < Minitest::Test
   def test_checkout_instantiation
     co = Checkout.new
     assert_instance_of Checkout, co
+  end
+
+  def test_basket_starts_empty
+    co = Checkout.new
+    assert co.basket.empty?
+  end
+
+  def test_scan_adds_a_product_to_the_basket
+    co = Checkout.new
+    chai = Product.new("Chai", "CH1", 3.11)
+
+    co.scan(chai)
+    assert_equal 1, co.basket.count
+    assert_equal "Chai", co.basket.first.name
+    assert_equal "CH1", co.basket.first.code
+    assert_equal 3.11, co.basket.first.price
   end
 end
